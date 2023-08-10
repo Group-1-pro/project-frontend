536158213dcf36596d3f4e446e612a87c30a50b4
Sara-Alkhateeb <sarajamal11399@yahoo.com>
2023-08-10 09:59:17 +0300

    fav updated - login done

--- components/FavoritesList.js
+++ components/FavoritesList.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '../contexts/auth';
-import LoginForm from '../components/LoginForm' ;
+import LoginForm from '../components/LoginForm';
 
 export default function FavoritesList() {
   const { tokens, login, user } = useAuth();
   const [fav_posts, favSetPosts] = useState([]);
   const baseUrl = 'http://127.0.0.1:8000/';
 
-  const fetchFavPostsData = async (id) => {
+  const fetchFavPostsData = async () => {
     try {
       const response = await fetch(baseUrl + `wanderhands/favorites`, {
         headers: {
@@ -29,6 +29,17 @@ export default function FavoritesList() {
     fetchFavPostsData();
   }, []);
 
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const handleContactPost = (postId) => {
+    const selected = fav_posts.find((post) => post.post.id === postId);
+    setSelectedPost(selected);
+  };
+
+  const handleCloseContact = () => {
+    setSelectedPost(null);
+  };
+
   return (
     <div>
       {user ? (
@@ -37,16 +48,20 @@ export default function FavoritesList() {
             <div key={post.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
               <div className="bg-white rounded-lg shadow-md p-6">
                 <img
-                  src={post.image}
+                  src={
+                    post.post.images.length > 0
+                      ? post.post.images[0].image
+                      : '/default-placeholder.png'
+                  }
                   alt={`Post ${post.id}`}
                   className="w-full h-32 object-cover mb-4 rounded-lg"
                 />
-                <h2 className="text-lg font-semibold mb-2">{post.title}</h2>
-                <p className="text-gray-600">{post.description}</p>
+                <h2 className="text-lg font-semibold mb-2">{post.post.title}</h2>
+                <p className="text-gray-600">{post.post.description}</p>
 
                 <div className="mt-4 flex justify-between">
                   <button
-                    onClick={() => handleContactPost(post.id)}
+                    onClick={() => handleContactPost(post.post.id)}
                     className="px-4 py-2 bg-blue-500 text-white rounded-md"
                   >
                     Contact
@@ -55,8 +70,28 @@ export default function FavoritesList() {
               </div>
             </div>
           ))}
+          {selectedPost && (
+            <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-black">
+              <div className="bg-white rounded-lg shadow-lg p-6 max-w-md">
+                <h2 className="text-xl font-semibold mb-2">
+                  Contact Information for "{selectedPost.post.title}"
+                </h2>
+                <p className="text-gray-700">Email: {selectedPost.post.email}</p>
+                <p className="text-gray-700">Phone: {selectedPost.post.phone}</p>
+                {/* Add more contact information here if needed */}
+                <button
+                  onClick={handleCloseContact}
+                  className="mt-4 px-4 py-2 bg-gray-600 text-white rounded-md"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
         </div>
-        ): ( <LoginForm onLogin={login} />)}
-      </div>
-  )
+      ) : (
+        <LoginForm onLogin={login} />
+      )}
+    </div>
+  );
 }
