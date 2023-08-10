e05b6a23c98ccd8407913f34de718e753e9cb169
asfantala <22037049@student.ltuc.com>
2023-08-10 07:25:00 +0000

    handled sign up

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import { useState } from 'react';
+import { useAuth } from '../contexts/auth';
+import { useRouter } from 'next/router';
 
-const SignupForm = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Add your form submission logic here
+const SignUpPage = () => {
+  const auth = useAuth();
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    await auth.signUp(username, email, password);
+    router.push('/login');
   };
 
   return (
-    <form className="w-8/12 p-6 mx-auto my-8 bg-gray rounded-lg shadow-md" onSubmit={handleSubmit}>
-      <fieldset className="flex flex-col" autoComplete="off">
-        <label htmlFor="username" className="mt-2">Username</label>
-        <input name="username" className="mt-2 p-2 rounded" placeholder="User Name" />
-        <label htmlFor="email" className="mt-2">Email</label>
-        <input type="email" name="email" className="mt-2 p-2 rounded" placeholder="Email" />
-        <label htmlFor="password" className="mt-4">Password</label>
-        <input type="password" name="password" className="p-2 rounded" placeholder="Password" />
-        <button className="px-6 py-3 mt-6 font-bold text-white bg-blue-300 rounded-lg shadow-md hover:bg-blue-400" type="submit">
-          Sign Up
-        </button>
-      </fieldset>
-    </form>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow">
+        <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
+        {auth.error && <p className="text-red-500 mb-4">{auth.error}</p>}
+        <form onSubmit={handleSignUp}>
+          <input
+            type="text"
+            className="w-full p-2 border mb-2 rounded"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="email"
+            className="w-full p-2 border mb-2 rounded"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="w-full p-2 border mb-2 rounded"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
-export default SignupForm;
+export default SignUpPage;
--- contexts/auth.js
+++ contexts/auth.js
@@ -1,11 +1,12 @@
 import { createContext, useContext, useState } from "react"
 import jwt from 'jsonwebtoken'
+import React, { useEffect } from 'react';
 
 
 //global
 
-const baseUrl = 'http://127.0.0.1:8000/' ;
-const tokenUrl = baseUrl+"api/token/";
+const baseUrl = 'http://127.0.0.1:8000/';
+const tokenUrl = baseUrl + "api/token/";
 
 const AuthContext = createContext();
 
@@ -18,55 +19,97 @@ export function useAuth() {
 }
 
 export function AuthProvider(props) {
+    const initialTokens = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('tokens')) : null;
 
     const [state, setState] = useState({
-        tokens: null,
+        tokens: initialTokens,
         user: null,
+        error: null,
         login,
         logout,
-    })
-
-    async function login(username, password) {
-        // const response =  axios.post(url,{username,password})
-
-        const options = {
-            method: "POST",
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' }
-
-        }
-
-        const response = await fetch(tokenUrl, options)
-        const data = await response.json()
-        
-
-
-        console.log("data", data)
-
-        const decodedAccess = jwt.decode(data.access)
-        console.log("decoded", decodedAccess)
-
-        const newState = {
-            tokens: data,
-            user: {
+        signUp,
+    });
+    useEffect(() => {
+        if (state.tokens) {
+            const decodedAccess = jwt.decode(state.tokens.access);
+            const user = {
                 username: decodedAccess.username,
                 email: decodedAccess.email,
                 id: decodedAccess.user_id
-            }
+            };
+            setState(prevState => ({ ...prevState, user }));
         }
+    }, [state.tokens]);
 
-        setState(prevState => ({ ...prevState, ...newState }));
+
+    async function login(username, password) {
+        // const response =  axios.post(url,{username,password})
+        try {
+            const options = {
+                method: "POST",
+                body: JSON.stringify({ username, password }),
+                headers: { 'Content-Type': 'application/json' }
+
+            }
+
+            const response = await fetch(tokenUrl, options)
+            const data = await response.json()
+
+
+
+            console.log("data", data)
+
+            const decodedAccess = jwt.decode(data.access)
+            console.log("decoded", decodedAccess)
+
+            const newState = {
+                tokens: data,
+                user: {
+                    username: decodedAccess.username,
+                    email: decodedAccess.email,
+                    id: decodedAccess.user_id
+                }
+            }
+
+            setState(prevState => ({ ...prevState, ...newState }));
+        } catch (error) {
+            console.error("Login error:", error);
+            setState(prevState => ({ ...prevState, error: error.message }));
+        }
 
 
     }
+    async function signUp(username, email, password) {
+        try {
+            const options = {
+                method: "POST",
+                body: JSON.stringify({ username, email, password }),
+                headers: { 'Content-Type': 'application/json' }
+            };
+
+            const response = await fetch(baseUrl + 'accounts/signup/', options);
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.detail || "Sign-up failed");
+            }
+
+            // Auto-login the user after successful sign-up
+            // await login(username, password);
+        }
+        catch (error) {
+            console.error("Sign-up error:", error);
+            setState(prevState => ({ ...prevState, error: error.message }));
+        }
+    }
 
     function logout() {
-        const newState = {
+        localStorage.removeItem('tokens');
+        setState(prevState => ({
+            ...prevState,
             tokens: null,
             user: null,
-        }
-
-        setState(prevState => ({ ...prevState, ...newState }));
+        }));
     }
 
     return (
