2a4ec66abbe5271ae36d2133df8e19c695df2af5
Mohammad-Shahin23 <mohammad.shahin2221@gmail.com>
2023-08-13 06:34:01 +0000

    fixing the post form

--- components/PostForm.js
+++ components/PostForm.js
@@ -410,7 +410,7 @@ export default function PostForm({ setShowForm }) {
                             <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                         <div className="flex items-center">
-                            <label htmlFor="images" className="relative w-40 mx-3 font-medium bg-orange-400 border border-orange-600 rounded-md outline-none cursor-pointer text-amber-900 h-7 hover:text-red-600">
+                            <label htmlFor="images" className="text-black bg-orange-500 rounded-md">
                                 <span className="font-bold p -2">Upload a file</span>
                                 <input
                                     id="images"
