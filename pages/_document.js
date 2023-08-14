2c4f3e8be44221ec9b76d232f7f301d99385ad61
Esmail-Jawabreh <esmailjawabreh@gmail.com>
2023-08-14 22:14:54 +0000

    new changes

--- pages/_document.js
+++ pages/_document.js
@@ -8,12 +8,16 @@ export default function Document() {
 
       <body>
 
-        <Main />
+        <div className="bodyContainer">
+          
+          <Main />
 
-        <NextScript />
+          <NextScript />
 
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
-        <script src="https://kit.fontawesome.com/28179b0d71.js"></script>
+          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
+          <script src="https://kit.fontawesome.com/28179b0d71.js"></script>
+          
+        </div>
 
       </body>
 
