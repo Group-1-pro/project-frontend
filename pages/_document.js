f5141acf9410681e7511e197f5acc9f2de802cc6
Esmail-Jawabreh <esmailjawabreh@gmail.com>
2023-08-15 12:04:23 +0000

    new changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-
-      <Head />
-
-      <body>
-
-        <div className="bodyContainer">
-          
-          <Main />
-
-          <NextScript />
-
-          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
-          <script src="https://kit.fontawesome.com/28179b0d71.js"></script>
-          
-        </div>
-
-      </body>
-
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="logo.png" />
+        </Head>
+        <body>
+          <div className="bodyContainer">
+            <Main />
+            <NextScript />
+            <link
+              rel="stylesheet"
+              href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+              integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+              crossOrigin="anonymous"
+            />
+            <script src="https://kit.fontawesome.com/28179b0d71.js"></script>
+          </div>
+        </body>
+      </Html>
+    );
+  }
 }
