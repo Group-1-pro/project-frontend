af9ec3e7904b114dd412e0789ee041c371cba5b6
asfantala <22037049@student.ltuc.com>
2023-08-15 21:29:37 +0000

    final edit inshallah

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -29,13 +29,13 @@ const SignUpPage = () => {
     <div className="md:flex md:h-screen">
       <div className="flex items-center justify-center py-10 md:w-1/2">
         {/* Your logo elements and styling can go here */}
-        <a  href="/" >
-            <img src="/lesspading-removebg-preview.png"  />
-          </a>
+        <a href="/" >
+          <img src="/lesspading-removebg-preview.png" />
+        </a>
       </div>
       <div className="flex items-center justify-center py-10 md:w-1/2">
         <form className="w-full max-w-md p-6 bg-white rounded-lg shadow">
-          <h1 className="mb-1 text-2xl font-bold text-gray-800">Sign Up</h1>
+          <h1 className="mb-1 text-2xl font-bold text-[#7E1717]">Sign Up</h1>
           <p className="text-sm font-normal text-gray-600 mb-7">
             It's quick and easy.
           </p>
@@ -67,7 +67,7 @@ const SignUpPage = () => {
             <input className="pl-2 font-semibold text-gray-600 border-none outline-none"
               type="text" name="" id="" placeholder="Email Address"
               value={email}
-              onChange={(e) => setEmail(e.target.value)} 
+              onChange={(e) => setEmail(e.target.value)}
               style={{
                 paddingLeft: '0.5rem',
                 outline: 'none',
@@ -75,7 +75,7 @@ const SignUpPage = () => {
                 width: '-webkit-fill-available',
                 borderRadius: '0.5rem',
 
-              }}/>
+              }} />
           </div>
           <div className="flex items-center px-3 py-2 border-2 rounded-2xl">
             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-gray-400" viewBox="0 0 20 20"
