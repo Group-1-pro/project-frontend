6b666a2c9117a3ef025995e56b103246ab3304b8
saifobe <obeidatsaif93@gmail.com>
2023-08-15 21:18:11 +0000

    post detail style

--- components/Loading.js
+++ components/Loading.js
@@ -4,7 +4,11 @@ import React from 'react';
 const Loading = () => {
 
     return (
-        <div class="dots-5"></div>
+        <div className="flex items-center justify-center space-x-2">
+        <div className="w-4 h-4 rounded-full animate-pulse dark:bg-red-800"></div>
+        <div className="w-4 h-4 rounded-full animate-pulse dark:bg-red-800"></div>
+        <div className="w-4 h-4 rounded-full animate-pulse dark:bg-red-800"></div>
+    </div>
         );
 }
 
